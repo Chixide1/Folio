b8084dbb8a0bf7956857ca4ab51db669ee536a42
Chikezie <chixide12@gmail.com>
2025-06-28 13:18:21 +0100

    - Loading now use height 100% instead of taking up the whole screen

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@
 
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-full py-10">
       <Spinner className="size-10"  />
     </div>
   )
