bed4de5575d48c4cf9606f02906a637bcfb40449
Chikezie <chixide12@gmail.com>
2025-07-04 19:04:45 +0100

    Style - Made no articles section text-centered

--- src/components/features/blog/no-articles-section.tsx
+++ src/components/features/blog/no-articles-section.tsx
@@ -3,16 +3,15 @@ import {cn} from "@/lib/utils";
 import {ComponentPropsWithoutRef} from "react";
 import {StripesBg} from "@/components/ui/stripes";
 import {MdSearchOff} from "react-icons/md";
+import {InfoSection} from "@/components/ui/info-section";
 
 export function NoArticlesSection({className, ...props}: ComponentPropsWithoutRef<"div">) {
   return (
-    <StripesBg className={cn("p-8 border-b", className)} {...props}>
-      <div className="ring-1 ring-accent/20 max-w-2xl mx-auto dark:bg-card bg-white backdrop-blur-sm flex items-center flex-col justify-center rounded-md p-6 border shadow-md">
+    <InfoSection className={cn("p-8 border-b", className)} {...props}>
         <Clock className="h-8 w-8 text-accent mb-3" />
         <h2 className="font-semibold mb-2 text-lg">Stay Tuned</h2>
-        <p className="text-secondary">Quality content is being carefully prepared and will be available soon!</p>
-      </div>
-    </StripesBg>
+        <p className="text-secondary text-center text-pretty">Quality content is being carefully prepared and will be available soon!</p>
+    </InfoSection>
   )
 }
 
